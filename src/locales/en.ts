1e58485ef00df71fa9d67bc32bf68440d9abdf9e
Double Colon <djdavid98@protonmail.com>
2022-04-18 01:17:53 +0200

    New translations en.ts (English, United Kingdom)

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,19 +1,39 @@
 import { Localization } from '../types/localization.js';
 import { MessageTimestampFormat } from '../utils/time.js';
-
 export const enLocale: Localization = {
   commands: {
     timestamp: {
       name: 'timestamp',
       description: 'Can be used to generate the code for timestamps is all formats',
       options: {
-        year: { name: 'year', description: 'Specify the year (defaults to current year)' },
-        month: { name: 'month', description: 'Specify the month (defaults to current month)' },
-        day: { name: 'day', description: 'Specify the day (defaults to current day)' },
-        hour: { name: 'hour', description: 'Specify the hour (defaults to current hour)' },
-        minute: { name: 'minute', description: 'Specify the minute (defaults to current minute)' },
-        second: { name: 'second', description: 'Specify the second (defaults to 0)' },
-        timezone: { name: 'timezone', description: 'Specify the timezone (defaults to GMT)' },
+        year: {
+          name: 'year',
+          description: 'Specify the year (defaults to current year)'
+        },
+        month: {
+          name: 'month',
+          description: 'Specify the month (defaults to current month)'
+        },
+        day: {
+          name: 'day',
+          description: 'Specify the day (defaults to current day)'
+        },
+        hour: {
+          name: 'hour',
+          description: 'Specify the hour (defaults to current hour)'
+        },
+        minute: {
+          name: 'minute',
+          description: 'Specify the minute (defaults to current minute)'
+        },
+        second: {
+          name: 'second',
+          description: 'Specify the second (defaults to 0)'
+        },
+        timezone: {
+          name: 'timezone',
+          description: 'Specify the timezone (defaults to GMT)'
+        },
         format: {
           name: 'format',
           description: 'Returns only the specified format in the response',
@@ -24,10 +44,10 @@ export const enLocale: Localization = {
             [MessageTimestampFormat.LONG_TIME]: 'long time',
             [MessageTimestampFormat.SHORT_FULL]: 'date and time',
             [MessageTimestampFormat.LONG_FULL]: 'weekday, date and time',
-            [MessageTimestampFormat.RELATIVE]: 'relative',
-          },
-        },
-      },
-    },
-  },
+            [MessageTimestampFormat.RELATIVE]: 'relative'
+          }
+        }
+      }
+    }
+  }
 };
