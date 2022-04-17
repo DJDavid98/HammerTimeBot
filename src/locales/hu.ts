d8b46886dac9f01edcfc6bc972fca7db9b57f798
Double Colon <djdavid98@protonmail.com>
2022-04-18 01:17:50 +0200

    New translations en.ts (Hungarian)

--- src/locales/hu.ts
+++ src/locales/hu.ts
@@ -1,22 +1,42 @@
 import { Localization } from '../types/localization.js';
 import { MessageTimestampFormat } from '../utils/time.js';
-
-export const huLocale: Localization = {
+export const enLocale: Localization = {
   commands: {
     timestamp: {
-      name: 'időbélyeg',
-      description: 'Kiírja egy adott időponthoz a beírandó kódot minden formátumhoz',
+      name: "időbélyeg",
+      description: "Kiírja egy adott időponthoz a beírandó kódot minden formátumhoz",
       options: {
-        year: { name: 'év', description: 'Add meg az évet (alapértelmezett: aktuális év)' },
-        month: { name: 'hónap', description: 'Add meg a hónapot (alapértelmezett: aktuális hónap)' },
-        day: { name: 'nap', description: 'Add meg az napot (alapértelmezett: aktuális nap)' },
-        hour: { name: 'óra', description: 'Add meg az órát (alapértelmezett: aktuális óra)' },
-        minute: { name: 'perc', description: 'Add meg az percet (alapértelmezett: aktuális perc)' },
-        second: { name: 'másodperc', description: 'Add meg az másodpercet (alapértelmezett: 0)' },
-        timezone: { name: 'időzóna', description: 'Add meg az időzónát (alapértelmezett: GMT)' },
+        year: {
+          name: "év",
+          description: "Add meg az évet (alapértelmezett: aktuális év)"
+        },
+        month: {
+          name: "hónap",
+          description: "Add meg a hónapot (alapértelmezett: aktuális hónap)"
+        },
+        day: {
+          name: "nap",
+          description: "Add meg az napot (alapértelmezett: aktuális nap)"
+        },
+        hour: {
+          name: "óra",
+          description: "Add meg az órát (alapértelmezett: aktuális óra)"
+        },
+        minute: {
+          name: "perc",
+          description: "Add meg az percet (alapértelmezett: aktuális perc)"
+        },
+        second: {
+          name: "másodperc",
+          description: "Add meg az másodpercet (alapértelmezett: 0)"
+        },
+        timezone: {
+          name: "időzóna",
+          description: "Add meg az időzónát (alapértelmezett: GMT)"
+        },
         format: {
-          name: 'formátum',
-          description: 'A formátumok közül csak a kértet adja vissza',
+          name: "formátum",
+          description: "A formátumok közül csak a kértet adja vissza",
           choices: {
             [MessageTimestampFormat.SHORT_DATE]: 'short date',
             [MessageTimestampFormat.LONG_DATE]: 'long date',
@@ -24,10 +44,10 @@ export const huLocale: Localization = {
             [MessageTimestampFormat.LONG_TIME]: 'long time',
             [MessageTimestampFormat.SHORT_FULL]: 'date and time',
             [MessageTimestampFormat.LONG_FULL]: 'weekday, date and time',
-            [MessageTimestampFormat.RELATIVE]: 'relative',
-          },
-        },
-      },
-    },
-  },
+            [MessageTimestampFormat.RELATIVE]: 'relative'
+          }
+        }
+      }
+    }
+  }
 };
