b8078f8f9dc856ab18d23f7635a75a9ced2bfcf3
DJDavid98 <djdavid98@protonmail.com>
2025-05-01 12:09:29 +0200

    Fix Extract Timestamps not detecting format-less values

--- src/utils/extract-timestamps-from-strings.ts
+++ src/utils/extract-timestamps-from-strings.ts
@@ -5,7 +5,7 @@ export const extractTimestampsFromStrings = (input: string | string[]): string[]
   let matches: string[] = [];
   const content = Array.isArray(input) ? input : [input];
   content.forEach(item => {
-    const itemMatches = item.match(/<t:\d+:[dftDFRT]>/g) as string[] | null;
+    const itemMatches = item.match(/<t:\d+(?::[dftDFRT])?>/g) as string[] | null;
     if (itemMatches) {
       matches = [...matches, ...itemMatches];
     }
