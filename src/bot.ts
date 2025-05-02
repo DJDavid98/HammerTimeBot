fef98f5643c7282c9bf4b8c28cdfd8a7dd950359
DJDavid98 <djdavid98@protonmail.com>
2025-05-02 16:59:10 +0200

    Parallelize bot script initial setup

--- src/bot.ts
+++ src/bot.ts
@@ -6,16 +6,22 @@ import { initI18next } from './constants/locales.js';
 import { getApplicationEmojis } from './utils/get-application-emojis.js';
 
 (async () => {
-  const tzDataPath = join('.', 'node_modules', 'moment-timezone', 'data', 'packed', 'latest.json');
-  console.log(`Loading timezone data from ${tzDataPath}`);
-  const data = await fs.readFile(tzDataPath).then((contents) => JSON.parse(contents.toString()));
-  moment.tz.load(data);
-
-  console.log('Initializing i18n');
-  const i18next = await initI18next();
-
-  console.log('Getting application emoji data');
-  const emojiIdMap = await getApplicationEmojis();
+  const [, i18next, emojiIdMap] = await Promise.all([
+    (async () => {
+      const tzDataPath = join('.', 'node_modules', 'moment-timezone', 'data', 'packed', 'latest.json');
+      console.log(`Loading timezone data from ${tzDataPath}`);
+      const data = await fs.readFile(tzDataPath).then((contents) => JSON.parse(contents.toString()));
+      moment.tz.load(data);
+    })(),
+    (async () => {
+      console.log('Initializing i18n');
+      return initI18next();
+    })(),
+    (async () => {
+      console.log('Getting application emoji data');
+      return getApplicationEmojis();
+    })(),
+  ]);
 
   console.log('Creating client');
   await createClient({ i18next, emojiIdMap });
