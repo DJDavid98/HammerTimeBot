e2cf910687bdc489dd6704e0c365295819df1a6f
WentTheFox <mail@went.tf>
2025-05-28 01:26:03 +0200

    Ease length restrictions on /snowflake value option

--- src/options/metadata/snowflake.option-meta.ts
+++ src/options/metadata/snowflake.option-meta.ts
@@ -1,10 +1,7 @@
 import { StringOptionMetadata } from '../../types/bot-interaction.js';
 import { ApplicationCommandOptionType } from 'discord-api-types/v10';
 
-const exampleSnowflake = '952258283882819595';
-
 export const snowflakeOptionMeta: StringOptionMetadata = {
   type: ApplicationCommandOptionType.String,
-  min_length: exampleSnowflake.length,
-  max_length: exampleSnowflake.length,
+  min_length: 1,
 };
